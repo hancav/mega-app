04dbdc3b776e850cbc8b28a1f3f8409530efd3ec
Hugo Cavalaria <hc@mail.pt>
2024-04-06 02:41:41 +0100

    add mailhog to network

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+//Artisan::command('backup:clean')->daily()->at('01:00');
+//Artisan::command('backup:run')->daily()->at('01:30');
