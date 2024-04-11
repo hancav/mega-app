8bca8f52f528226c2ddf5b7283a73cd0fa89d333
Hugo Cavalaria <hc@mail.pt>
2024-04-11 11:53:25 +0100

    fix web routes

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,12 @@
 <?php
-
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\Dashboard\DashboardController;
 
 Route::get('/', [IndexController::class, 'home'])->name('index');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'home'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
