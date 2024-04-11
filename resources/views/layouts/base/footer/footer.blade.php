6f447ec0c92789d7685e558fc49f2396f5dd8d97
Hugo Cavalaria <hc@mail.pt>
2024-04-11 17:34:57 +0100

    refactor footer

--- resources/views/layouts/base/footer/footer.blade.php
+++ resources/views/layouts/base/footer/footer.blade.php
@@ -1,24 +1,9 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
 <div class="navbar-rtp">
   <ul>
-    <li>
-      <a class="logo-rtp show-mobile-inline" href="javascript:;" onclick="slideToggle()">
-        <svg width="64" height="15" viewBox="0 0 2000 450" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" clip-rule="evenodd" d="M0 317.258C0 334.238 14.189 348.11 31.795 348.11H829.065C846.641 348.11 860.936 334.238 860.936 317.258C860.936 300.382 846.641 286.593 829.065 286.593H31.795C14.189 286.593 0 300.382 0 317.258ZM0 225.058C0 242.042 14.189 255.914 31.795 255.914H829.065C846.641 255.914 860.936 242.042 860.936 225.058C860.936 208.186 846.641 194.393 829.065 194.393H31.795C14.189 194.393 0 208.186 0 225.058ZM0 132.861C0 149.846 14.189 163.714 31.795 163.714H829.065C846.641 163.714 860.936 149.846 860.936 132.861C860.936 115.99 846.641 102.197 829.065 102.197H31.795C14.189 102.197 0 115.99 0 132.861ZM92.3164 409.571C92.3164 426.617 106.53 440.307 124.082 440.307H736.854C754.536 440.307 768.62 426.617 768.62 409.571C768.62 392.578 754.536 378.789 736.854 378.789H124.082C106.53 378.789 92.3164 392.578 92.3164 409.571ZM124.082 10H736.854C754.536 10 768.62 23.79 768.62 40.783C768.62 57.829 754.536 71.518 736.854 71.518H124.082C106.53 71.518 92.3164 57.829 92.3164 40.783C92.3164 23.79 106.53 10 124.082 10Z" fill="white"/>
-        <path fill-rule="evenodd" clip-rule="evenodd" d="M1321.88 157.17V101.533H1629.98V157.17H1507.04V349.327H1444.98V157.17H1321.88ZM1743.31 287.203V349.327H1682V101.533H1904.55C1957.32 101.533 2000 143.08 2000 194.393C2000 245.674 1957.32 287.203 1904.55 287.203H1743.31ZM1743.31 157.17V231.531H1900.27C1921.39 231.531 1938.29 214.765 1938.29 194.393C1938.29 173.889 1921.39 157.17 1900.27 157.17H1743.31ZM1013.77 287.635V349.327H952.451V101.533H1181.07C1233.78 101.533 1276.48 143.08 1276.48 194.361C1276.48 227.455 1258.67 256.586 1231.89 272.699L1276.48 349.327H1209.27L1172.88 287.635H1013.77ZM1013.77 157.17V231.562H1176.75C1197.93 231.562 1214.83 214.742 1214.83 194.361C1214.83 173.881 1197.93 157.17 1176.75 157.17H1013.77Z" fill="white"/>
-        </svg>
-        <i class="chevron-down-rtp"></i>
-      </a>
-      <a class="logo-rtp show-desktop-inline" href="https://www.rtp.pt/" target="_blank">
-        <svg width="64" height="15" viewBox="0 0 2000 450" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" clip-rule="evenodd" d="M0 317.258C0 334.238 14.189 348.11 31.795 348.11H829.065C846.641 348.11 860.936 334.238 860.936 317.258C860.936 300.382 846.641 286.593 829.065 286.593H31.795C14.189 286.593 0 300.382 0 317.258ZM0 225.058C0 242.042 14.189 255.914 31.795 255.914H829.065C846.641 255.914 860.936 242.042 860.936 225.058C860.936 208.186 846.641 194.393 829.065 194.393H31.795C14.189 194.393 0 208.186 0 225.058ZM0 132.861C0 149.846 14.189 163.714 31.795 163.714H829.065C846.641 163.714 860.936 149.846 860.936 132.861C860.936 115.99 846.641 102.197 829.065 102.197H31.795C14.189 102.197 0 115.99 0 132.861ZM92.3164 409.571C92.3164 426.617 106.53 440.307 124.082 440.307H736.854C754.536 440.307 768.62 426.617 768.62 409.571C768.62 392.578 754.536 378.789 736.854 378.789H124.082C106.53 378.789 92.3164 392.578 92.3164 409.571ZM124.082 10H736.854C754.536 10 768.62 23.79 768.62 40.783C768.62 57.829 754.536 71.518 736.854 71.518H124.082C106.53 71.518 92.3164 57.829 92.3164 40.783C92.3164 23.79 106.53 10 124.082 10Z" fill="white"/>
-        <path fill-rule="evenodd" clip-rule="evenodd" d="M1321.88 157.17V101.533H1629.98V157.17H1507.04V349.327H1444.98V157.17H1321.88ZM1743.31 287.203V349.327H1682V101.533H1904.55C1957.32 101.533 2000 143.08 2000 194.393C2000 245.674 1957.32 287.203 1904.55 287.203H1743.31ZM1743.31 157.17V231.531H1900.27C1921.39 231.531 1938.29 214.765 1938.29 194.393C1938.29 173.889 1921.39 157.17 1900.27 157.17H1743.31ZM1013.77 287.635V349.327H952.451V101.533H1181.07C1233.78 101.533 1276.48 143.08 1276.48 194.361C1276.48 227.455 1258.67 256.586 1231.89 272.699L1276.48 349.327H1209.27L1172.88 287.635H1013.77ZM1013.77 157.17V231.562H1176.75C1197.93 231.562 1214.83 214.742 1214.83 194.361C1214.83 173.881 1197.93 157.17 1176.75 157.17H1013.77Z" fill="white"/>
-        </svg>
-        <i class="chevron-down-rtp"></i>
-      </a>
-    <li>
     <ul class="dropdown-rtp" id="dropdown-rtp">
       <li><a href="https://localhost" target="_blank">Link 1</a></li>
       <li><a href="https://localhost" target="_blank">Link 2</a></li>
@@ -33,43 +18,9 @@
   </ul>
 </div>
 
-<div class="bg-takeover">
-  <div class="container takeover">
-    <div class="row">
-      <div id='leaderboard'></div>
-    </div>
-  </div>
-  <div class="layer popover" id='multiusos'></div>
-  <div class="layer popover" id='multiusos-oop'></div>
-</div>
-
 <style>
 body{
   -webkit-font-smoothing: antialiased;
-}
-
-.bg-takeover>.container {
-  position: relative;
-  max-width: none !important;
-}
-
-#leaderboard{
-  line-height: 0 !important;
-  margin: 0 auto;
-  text-align: center;
-}
-
-.layer.popover {
-  position: fixed;
-  z-index: 100;
-  top: 0;
-  left: 0;
-  width: 1px !important;
-  height: 1px !important;
-  margin: 0 !important;
-  max-width: none !important;
-  border: 0 !important;
-  background-color: transparent !important;
 }
 
 .navbar-rtp{
@@ -91,20 +42,6 @@ body{
   display: inline-block;
 }
 
-.navbar-rtp .logo-rtp{
-  opacity: 0.5;
-  position: relative;
-  top:3px;
-  margin-right: 10px;
-  text-decoration:none;
-}
-
-.logo-rtp.show-mobile-inline{
-  padding:7.5px;
-  top:0;
-  margin:0;
-}
-
 .navbar-rtp .dropdown-rtp li a{
   font-size: 12px;
   font-weight: 500;
@@ -114,73 +51,6 @@ body{
   display: inline-block;
   color: #fff;
   opacity: 0.5;
-}
-
-.navbar-rtp .dropdown-rtp li a:hover,
-.navbar-rtp .logo-rtp:hover{
-  opacity: 0.8;
-}
-
-.chevron-down-rtp {
-  box-sizing: border-box;
-  position: relative;
-  display: none;
-  width: 8px;
-  height: 8px;
-  border: 2px solid transparent;
-  border-radius: 100px;
-  color: #fff;
-  top: -4px
-}
-
-.chevron-down-rtp::after {
-  content: "";
-  display: block;
-  box-sizing: border-box;
-  position: absolute;
-  width: 8px;
-  height: 8px;
-  border-bottom: 2px solid;
-  border-right: 2px solid;
-  transform: rotate(45deg);
-  top: -4px;
-  transform-origin: 5px 6px;
-  transition: transform 0.3s ease-in-out;
-}
-
-.chevron-down-rtp.rotate-chevron::after{
-  transform: rotate(-135deg);
-}
-
-.sso-wraper{
-  position: absolute;
-  top: 0;
-  right: 0;
-  background-color: #232323;
-  color: #A0ACB3;
-  height: 100%;
-  padding: 0 11px;
-  font-size: 12px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  text-decoration: none;
-}
-
-.sso-wraper span {
-  margin-right: 9px;
-}
-
-.sso-user{
-  font-size: 8px;
-  font-weight: 600;
-  line-height: 20px;
-  width: 20px;
-  height: 20px;
-  text-align: center;
-  color: #fff;
-  background-color: #5381F8;
-  border-radius: 50%;
 }
 
 @media (min-width: 992px){
@@ -233,10 +103,6 @@ body{
     opacity: 0.8;
   }
 
-  .chevron-down-rtp{
-    display: inline-block;
-  }
-
   .show-mobile-inline{
     display: inline-block !important;
   }
@@ -251,102 +117,4 @@ body{
   }
 
 }
-
-</style>
-
-<style>
-#sso-menu{    
-  border-top-left-radius: 0 !important;
-  border-top-right-radius: 0 !important;
-  top: 35px !important;
-  right: 0 !important;
-}
-
-#sso-menu p{
-  font-size: 12px !important;
-  line-height: 16px !important;
-  color: #fff;
-  text-align:left;
-}
-
-#sso-menu button{
-  display:block;
-  border:0;
-  background-color: transparent;
-  color: #fff !important;
-  font-size:12px;
-  font-weight:400;
-  padding: 10px 0 10px 28px;
-  font-family: 'Inter', sans-serif;
-  position:relative;
-  cursor:pointer;
-  width: 100%;
-  text-align: left;
-  border-bottom: 1px dotted rgba(160,178,200,0.3);
-}
-
-#sso-menu button:last-child{
-  border-bottom:0;
-}
-
-#sso-menu button:hover{
-  color: #0074FF;
-}
-
-#sso-menu button::before{
-  content: ' ';
-  background-repeat: no-repeat;
-  position:absolute;
-  left: 0;
-  top:7px;
-  display:block;
-  width: 20px;
-  height: 20px;
-}
-
-#sso-menu button.sso-menu-logout::before{
-  background-image: url('/common/navbar/img/logout_white.svg');
-}
-
-#sso-menu button.sso-menu-favorites::before{
-  background-image: url('/common/navbar/img/star_white.svg');
-}
-
-#sso-menu button.sso-menu-manage::before{
-  background-image: url('/common/navbar/img/manage_white.svg');
-}
-
-#sso-menu button.sso-menu-manage:hover::before{
-  background-image: url('/common/navbar/img/manage_color.svg');
-}
-
-#sso-menu button.sso-menu-favorites:hover::before{
-  background-image: url('/common/navbar/img/star_color.svg');
-}
-
-#sso-menu button.sso-menu-logout:hover::before{
-  background-image: url('/common/navbar/img/logout_color.svg');
-}
-
-@media (max-width: 1022px) {
-
-  #sso-menu{
-    background-color: rgba(36, 35, 37, 0.9) !important;
-    width:100% !important;
-    position: fixed !important;
-    left: 0 !important;
-  }
-
-  #sso-menu button,
-  #sso-menu p{
-    font-size: 22px;
-    font-weight:500;
-  }
-
-  #sso-menu button::before{
-    top: 16px !important;
-  }
-
-}
-
 </style>
