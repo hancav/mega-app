71e8e2be9f0af941f40bbcc8ccc3bba82c993f35
Hugo Cavalaria <hc@mail.pt>
2024-04-12 11:42:08 +0100

    remove top body scripts and simpler header

--- resources/views/layouts/base/scripts/top_body_scripts.blade.php
+++
@@ -1,4 +0,0 @@
-{{--== Top Scripts ==--}}
-
-{{--= RTP Player Min =--}}
-{{-- <script src="//{{config('rtp.player_common.domain')}}/script.min.js?v={{config('scripts.rtp-player.version')}}"></script> --}}
