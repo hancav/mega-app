b4c2b824ceeac4ae20ad146c42b0be43b5c16794
Hugo Cavalaria <hc@mail.pt>
2024-04-18 18:15:50 +0100

    add routes for terms and welcome

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,12 @@ use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [IndexController::class, 'index'])->name('index');
+Route::view('/', 'welcome')->name('welcome');
+//Route::get('/', [IndexController::class, 'index'])->name('index');
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::view('/terms', 'terms')->name('terms');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
