d50182d3e4d2b6e9617da59dacf311b7e72415a4
Hugo Cavalaria <hc@mail.pt>
2024-04-18 18:37:54 +0100

    update terms

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -1,14 +1,14 @@
 <x-app-layout>
     <div class="mx-auto my-16 max-w-7xl px-6 lg:px-8">
         <a href="{{ route('welcome') }}" class="-mt-10 mb-12 flex items-center text-slate-400 hover:underline" wire:navigate>
-            {{-- <x-icons.chevron-left class="size-4" /> --}}
+            <x-icons.chevron-left class="size-4" />
             <span>Back</span>
         </a>
 
         <div class="mt-6">
             <div class="prose prose-slate prose-invert mx-auto max-w-4xl">
                 <h1>Terms of Service</h1>
-                <p><strong>Last Updated: 19 Feb 2024</strong></p>
+                <p><strong>Last Updated: 18 Apr 2024</strong></p>
 
                 <p>
                     Welcome to Mega-Site, a digital platform designed to streamline your online presence by allowing users to create a personalized and
@@ -16,73 +16,7 @@
                     you agree to be bound by these Terms of Service (“Terms”).
                 </p>
 
-                <h2>1. Acceptance of Terms</h2>
-                <p>
-                    By creating an account, accessing, or using the Services, you signify your agreement to these Terms. If you do not agree to these
-                    Terms, you may not access or use the Services. Mega-Site reserves the right to modify or replace these Terms at any time at its sole
-                    discretion. Your continued use of the Services after any changes signifies your acceptance of the new Terms.
-                </p>
-
-                <h2>2. Use of Services</h2>
-                <p>
-                    <strong>a. Eligibility:</strong>
-                    You must be at least 16 years old to use the Services, in compliance with Portuguese law. By agreeing to these Terms, you
-                    represent and warrant that you are of legal age to form a binding contract.
-                </p>
-                <p>
-                    <strong>b. Mega-Site Account:</strong>
-                    You may need to register an account to access certain features of the Services. You are responsible for maintaining the
-                    confidentiality of your account and password.
-                </p>
-                <p>
-                    <strong>c. Acceptable Use:</strong>
-                    You agree not to use the Services for any unlawful purposes or in ways that could damage, disable, overburden, or impair the
-                    Services or interfere with any other party's use and enjoyment of the Services.
-                </p>
-
-                <h2>3. Content</h2>
-                <p>
-                    <strong>a. User Content:</strong>
-                    You are responsible for the content you provide, including compliance with applicable laws, rules, and regulations under
-                    Portuguese and EU law. You retain all rights in, and are solely responsible for, the User Content you post to Mega-Site.
-                </p>
-                <p>
-                    <strong>b. Rights Granted by You:</strong>
-                    By posting content to the Services, you grant Mega-Site a non-exclusive, worldwide, royalty-free, sublicensable, and transferable
-                    license to use, reproduce, distribute, prepare derivative works of, display, and perform the content in connection with the
-                    Services.
-                </p>
-
-                <h2>4. Intellectual Property</h2>
-                <p>
-                    The Services and original content (excluding User Content), features, and functionality are and will remain the exclusive property
-                    of Mega-Site and its licensors.
-                </p>
-
-                <h2>5. Termination</h2>
-                <p>
-                    Mega-Site may terminate or suspend your access to the Services immediately, without prior notice or liability, for any reason,
-                    including if you breach the Terms.
-                </p>
-
-                <h2>6. Disclaimers and Limitations of Liability</h2>
-                <p>
-                    The Services are provided on an “as is” and “as available” basis. Mega-Site expressly disclaims all warranties of any kind, whether
-                    express or implied, including, but not limited to, the warranties of merchantability, fitness for a particular purpose, and
-                    non-infringement. Mega-Site will not be liable for any indirect, incidental, special, consequential, or punitive damages resulting
-                    from your use of the Services.
-                </p>
-
-                <h2>7. Governing Law</h2>
-                <p>
-                    These Terms shall be governed and construed in accordance with the laws of Portugal and the regulations of the European Union,
-                    without regard to its conflict of law provisions.
-                </p>
-
-                <h2>8. Changes</h2>
-                <p>We reserve the right, at our sole discretion, to modify or replace these Terms at any time.</p>
-
-                <h2>9. Contact Us</h2>
+                <h2> Contact Us</h2>
                 <p>
                     If you have any questions about these Terms, please contact us at
                     <a href="mailto:team@mega-site.com">team@mega-site.com</a>
