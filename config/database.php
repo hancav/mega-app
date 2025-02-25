5a70e83c73e04037b36da6b0e0c372deb17fb69f
Hugo Cavalaria <hugo.cavalaria.aux@gmail.com>
2025-02-25 20:48:56 +0000

    sqlite

--- config/database.php
+++ config/database.php
@@ -33,6 +33,14 @@ return [
 
     'connections' => [
 
+        'connection_sqlite' => [
+            'driver' => 'sqlite',
+            'url' => env('DATABASE_URL'),
+            'database' => database_path('database.sqlite'),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        ],
+
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
