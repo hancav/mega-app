5e73c1269c0f28d76b030497714a430a0af83cdb
Hugo Cavalaria <hugo.cavalaria.aux@gmail.com>
2025-02-25 19:52:56 +0000

    merge

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -12,7 +12,7 @@ final class IndexController extends Controller
      * Home method
      *
      **/
-    public function index(): void
+    public function index()
     {
         // define view name
         $view = 'templates.index';
