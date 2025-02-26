8939f044aa9d0b1101e566372573a771c826c061
Hugo Cavalaria <hugo.cavalaria.aux@gmail.com>
2025-02-26 21:48:00 +0000

    icons

--- resources/views/layouts/base/base.blade.php
+++ resources/views/layouts/base/base.blade.php
@@ -5,6 +5,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
 
+    <!-- iOS icon meta tags -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('icons/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}">
+    <meta name="apple-mobile-web-app-title" content="{{ config('app.name') }}">
+    <meta name="application-name" content="{{ config('app.name') }}">
+    <meta name="theme-color" content="#ffffff">
+
     @section('metadata')
       <title>{{ config('app.name') }}</title>
     @show
