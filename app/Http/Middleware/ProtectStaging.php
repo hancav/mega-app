fddee33a6a27b949d87613fa982aec1a43227ff8
Hugo Cavalaria <hugo.cavalaria@rtp.pt>
2026-04-29 16:57:57 +0100

    refactor

--- app/Http/Middleware/ProtectStaging.php
+++ app/Http/Middleware/ProtectStaging.php
@@ -11,15 +11,12 @@ final class ProtectStaging
 {
     public function handle(Request $request, Closure $next)
     {
-        if (config('app.env') === 'staging') {
-            if (! isset($_SERVER['PHP_AUTH_USER']) ||
-                $_SERVER['PHP_AUTH_USER'] !== config('app.staging_username') ||
-                $_SERVER['PHP_AUTH_PW'] !== config('app.staging_password')
-            ) {
-                header('WWW-Authenticate: Basic realm="Staging Area"');
-                header('HTTP/1.0 401 Unauthorized');
-                exit('Unauthorized');
-            }
+        if (config('app.env') === 'staging' && (! isset($_SERVER['PHP_AUTH_USER']) ||
+            $_SERVER['PHP_AUTH_USER'] !== config('app.staging_username') ||
+            $_SERVER['PHP_AUTH_PW'] !== config('app.staging_password'))) {
+            header('WWW-Authenticate: Basic realm="Staging Area"');
+            header('HTTP/1.0 401 Unauthorized');
+            exit('Unauthorized');
         }
 
         return $next($request);
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
+    ->withMiddleware(function (Middleware $middleware): void {
         $middleware->web([App\Http\Middleware\ProtectStaging::class]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
